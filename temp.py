f050fbc8485d0b8cc9e286a72c8190fdf8035bf4
chungwon <pcw14057@gmail.com>
2024-06-13 16:38:47 +0900

    p1983

--- temp.py
+++ temp.py
@@ -2,12 +2,21 @@ T=int(input())
 
 for test_case in range(1,T+1):
     n,m=map(int(),input().split())
-    temp=list(map() for i in range)
-    fly=[]
+    temp=[list(map(int,input().split())) for i in range(n)]
+    result=[]
  
-    for i in range(n):
-        for j in range(n):
-            fly=[]
+    for i in range(n-m+1):
+        for j in range(n-m+1):
+            fly=0
+            for k in range(i+m):
+                for v in range(j+m):
+                    fly=fly+temp[k,v]
+            result.append(fly)
+    print(max(result))
+
+
+
+
 
 
 
