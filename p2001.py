db368984490ffadf19d78a257d28abacdfa0c637
chungwon <pcw14057@gmail.com>
2024-06-13 11:08:40 +0900

    p2001

--- p2001.py
+++ p2001.py
@@ -2,7 +2,7 @@ T=int(input())
 
 for test_case in range(1,T+1):
     n,m=map(int,input().split())
-    temp=[list(map(int,input().split()))]
+    temp=[list(map(int,input().split())) for i in range(n)]
 
     result=[]
     for i in range(n-m+1):
---
+++ temp.py
@@ -0,0 +1,13 @@
+T=int(input())
+
+for test_case in range(1,T+1):
+    n,m=map(int(),input().split())
+    temp=list(map() for i in range)
+    fly=[]
+ 
+    for i in range(n):
+        for j in range(n):
+            fly=[]
+
+
+
