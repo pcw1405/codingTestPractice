8e8df435ab34e5225aad3cc24d83221b4309ce31
chungwon <pcw14057@gmail.com>
2024-06-13 09:56:48 +0900

    first commit

---
+++ p2001.py
@@ -0,0 +1,17 @@
+T=int(input())
+
+for test_case in range(1,T+1):
+    n,m=map(int,input().split())
+    temp=[list(map(int,input().split()))]
+
+    result=[]
+    for i in range(n-m+1):
+        for j in range(n-m+1):
+            fly=0
+            for k in range(m):
+                for l in range(m):
+                    fly=fly+temp[i+k][i+l]
+                result.append(fly)
+    
+    print("#"+str(test_case),max(result))
+
