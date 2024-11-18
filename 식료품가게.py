bbf9073f62cf3f0262d4429b8ca1cfa4001f5311
chungwon <pcw14057@gmail.com>
2024-11-18 21:47:05 +0900

    식료품가게 문제 수정

--- 식료품가게.py
+++ 식료품가게.py
@@ -3,14 +3,15 @@ T = int(input())
 for tc in range(1, T + 1):
     n=int(input())
     temp=list(map(int,input().split()))
-    orignal=[]
-    for i in range(len(temp)):
-        x = float(temp[i]/(0.75))
-        if x in temp and x % 4==0:
-            orignal.append(temp[i])
+    price=[]
 
-    sorted_original=sorted(temp)
-    print("#{} {}".format(T + 1," ".join(map(str,sorted_original))))            
+    for i in range(len(temp)):
+        x = int(temp[i]/(0.75))
+        if x in temp and x % 4==0:
+            price.append(temp[i])
+
+    sorted_price=sorted(set(price))
+    print("#{} {}".format(tc," ".join(map(str,sorted_price))))            
         
         
 
